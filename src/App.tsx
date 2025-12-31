25dded878d43049348853e909b0c24d5148e47c7
FicelloScript <faissel.friouchen@gmail.com>
2025-12-31 07:07:25 +0100

    Add base Todo model and input layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+type Priority = "Urgente" | "Moyenne" | "Basse"
+
+type Todo = {
+  id: number;
+  text:string;
+  priority: Priority
+}
 
 function App() {
-
-
   return (
-    <div className="">
-      <button className="btn btn-accent">test</button>
+    <div className="flex justify-center">
+      <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
+        <div className="flex gap-4">
+          <input 
+            type="text"
+            className="input w-full"
+            placeholder="Ajouter une tâche..."
+          />
+          <select 
+            className="select w-full"
+          >
+            
+          </select>
+        </div>
+      </div>
     </div>
   )
 }
